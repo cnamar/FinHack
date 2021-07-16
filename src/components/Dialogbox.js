c441e8925070a714e7eb525342af58e2aab873da
cnamar <amarnathcn6112000@gmail.com>
2021-07-16 21:16:56 +0530

    start with table

--- src/components/Dialogbox.js
+++ src/components/Dialogbox.js
@@ -1,72 +1,84 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
-function Dialogbox() {
-  const [eventname, setEventName] = useState("");
-  const [date, setdate] = useState(1);
-  const [duration, setduration] = useState(5);
+import Eventstable from "./Eventstable";
+import "./Dialogbox.css";
+function Dialogbox(props) {
+  const [eventname, setEventName] = useState(props.name);
+  const [date, setdate] = useState(props.date);
+  const [duration, setduration] = useState(props.duration);
   const [events, setevents] = useState([]);
-  useEffect(() => {
-    console.log(events);
-  }, [events]);
-  const onsubmit = () => {
+  const [tablestatus, settablestatus] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
     setevents([
       ...events,
       { event: eventname, date: date, duration: duration },
     ]);
-    console.log(events);
+    settablestatus(true);
   };
   return (
     <div id="dialogbox" style={{ marginTop: "10rem" }}>
       <div class="container">
-        <form>
-          <label for="eventname">Event Name:</label>
-          <br />
-          <input
-            type="text"
-            id="eventname"
-            name="fname"
-            value={eventname}
-            onChange={(event) => {
-              setEventName(event.target.value);
-            }}
-          />
-          <br />
-          <label for="paymentdate">Date at which payment to be done:</label>
-          <br />
-          <input
-            type="number"
-            id="paymentdate"
-            min="1"
-            max="30"
-            name="paymentdate"
-            value={date}
-            onChange={(event) => {
-              setdate(event.target.value);
-            }}
-          />
-          <label for="duration">Duration(No of Years)</label>
-          <br />
-          <input
-            type="number"
-            id="duration"
-            name="duration"
-            value={duration}
-            onChange={(event) => {
-              setduration(event.target.value);
-            }}
-          />
-          <div className="form-group">
-            <div
-              value="save event"
-              className="btn btn-primary"
-              onClick={onsubmit}
-            >
-              save event
-            </div>
-          </div>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <label>Event Name:</label>
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="eventname"
+                    className="textfield1"
+                    value={eventname}
+                    onChange={(e) => setEventName(e.target.value)}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Date at which payment to be done:</label>
+                </td>
+                <td>
+                  <input
+                    type="date"
+                    name="paymentdate"
+                    className="textfield1"
+                    value={date}
+                    onChange={(e) => setdate(e.target.value)}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Duration(No of Years)</label>
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    name="duration"
+                    className="textfield1"
+                    value={duration}
+                    onChange={(e) => setduration(e.target.value)}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <input type="submit" value="Confirm" id="submit" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </form>
       </div>
+      {tablestatus ? <Eventstable /> : null}
     </div>
   );
 }
---
+++ src/components/Eventstable.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Eventstable() {
+  return <div>hello</div>;
+}
+
+export default Eventstable;
