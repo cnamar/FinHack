0abcd800e97be958192c8b52cfb951c785459fdb
Rj777pain <rejathtm@gmail.com>
2021-07-16 20:43:04 +0530

    form modified

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Dialogbox from './components/Dialogbox';
 import Kyccomponents from './components/kyc_component';
 
 function App() {
-  const [dialogboxstatus,setdialogboxstatus]=useState(false);
+  const [dialogboxstatus,setdialogboxstatus]=useState(true);
   return (
     <div className="App">
     <h1>Finhack</h1>
-    <div class="btn btn-success mt-6" onClick={()=>{setdialogboxstatus(true);}}>Payment Schedule</div>
-     {dialogboxstatus ? <Dialogbox/> : null}
+    <div className="btn btn-success mt-6" onClick={()=>{setdialogboxstatus(true);}}>Payment Schedule</div>
+     {dialogboxstatus ? <Dialogbox name={"aaa"} date={"2020-01-01"} duration={1}/> : null}
     
     </div>
   );
--- src/components/Dialogbox.js
+++ src/components/Dialogbox.js
@@ -1,22 +1,37 @@
 import { useState } from "react";
-
-function Dialogbox(){
-    const [eventname,setEventName]=useState("");
-    const [date,setdate]=useState(1);
-    const [duration,setduration]=useState(5);
+import './Dialogbox.css'
+function Dialogbox(props){
+    const [eventname,setEventName]=useState(props.name);
+    const [date,setdate]=useState(props.date);
+    const [duration,setduration]=useState(props.duration);
+    const handleSubmit= (e) => {
+        console.log(eventname+date+duration);
+        e.preventDefault();
+      }
     return(<div id="dialogbox" style={{marginTop:"10rem"}}>
-        <div class="container">
-         <form>
-  <label for="eventname">Event Name:</label><br/>
-  <input type="text" id="eventname" name="fname"/><br/>
-  <label for="paymentdate">Date at which payment to be done:</label><br/>
-  <input type="date" id="paymentdate" name="paymentdate"/>
-  <label for="duration">Duration(No of Years)</label><br/>
-  <input type="date" id="duration" name=""/>
-</form> 
-        </div>
-       
-        hello
+        <div className="container">
+         <form onSubmit={e => { handleSubmit(e)}}>
+             <table><tbody>
+                 <tr>
+                     <td><label  >Event Name:</label></td>
+                     <td><input type="text" name="eventname" className="textfield1"  value={eventname} onChange={e => setEventName(e.target.value)}/></td>
+                 </tr>
+                 <tr>
+                     <td><label >Date at which payment to be done:</label></td>
+                     <td><input type="date" name="paymentdate" className="textfield1" value={date} onChange={e => setdate(e.target.value)}/></td>
+                 </tr>
+                 <tr>
+                     <td><label>Duration(No of Years)</label></td>
+                     <td><input type="number" name="duration" className="textfield1" value={duration} onChange={e => setduration(e.target.value)}/></td>
+                 </tr>
+                 <tr>
+                     <td></td>
+                     <td><input type="submit" value="Confirm" id="submit"/></td>
+                 </tr>
+                 </tbody>
+             </table>
+        </form> 
+        </div>       
     </div>)
 }
 export default Dialogbox;
