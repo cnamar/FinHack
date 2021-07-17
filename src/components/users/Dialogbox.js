2d190789ab5e643adfb0843d323c5aa26cc0ba1a
Rj777pain <rejathtm@gmail.com>
2021-07-17 19:51:00 +0530

    material form implented

--- src/components/users/Dialogbox.js
+++ src/components/users/Dialogbox.js
@@ -1,6 +1,12 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import "./Dialogbox.css";
+// import "./Dialogbox.css";
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 function Dialogbox(props) {
   const [eventname, setEventName] = useState(props.name);
   const [date, setdate] = useState(props.date);
@@ -15,70 +21,107 @@ function Dialogbox(props) {
     
     
   };
+
+  const useStyles = makeStyles((theme) => ({
+  paper: {
+    
+    display: 'flex',
+    width:'400px',
+    
+    padding:'20px',
+    
+    paddingBottom:'20px',
+    elevation:'0px',
+    flexDirection: 'column',
+    alignItems: 'center',
+    
+    
+  },
+  
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    height:40
+  },
+  textField:{
+    height:'50px'
+  }
+}));
+
+const classes = useStyles();
   return (
-    <div id="dialogbox" style={{ marginTop: "10rem" }}>
-      <div className="container">
-        <form
+    
+
+      
+      <Container className={classes.paper}>
+        <h2></h2>
+      <form
+        className={classes.form}
           onSubmit={(e) => {
             handleSubmit(e);
           }}
         >
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  <label>Event Name:</label>
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="eventname"
-                    className="textfield1"
-                    value={eventname}
-                    onChange={(e) => setEventName(e.target.value)}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label>Date at which payment to be done:</label>
-                </td>
-                <td>
-                  <input
-                    type="date"
-                    name="paymentdate"
-                    className="textfield1"
-                    value={date}
-                    onChange={(e) => setdate(e.target.value)}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label>Duration(No of Years)</label>
-                </td>
-                <td>
-                  <input
-                    type="number"
-                    name="duration"
-                    className="textfield1"
-                    value={duration}
-                    onChange={(e) => setduration(e.target.value)}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>
-                  <input type="submit" value="Confirm" id="submit" />
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <TextField
+            className={classes.textField}
+            // variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            
+            label="Event Name"
+            name="eventname"
+            value={eventname}
+            onChange={(e) => setEventName(e.target.value)}
+            autoComplete="Eventname"
+            autoFocus
+          />
+          <TextField
+            className={classes.textField}
+            // variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            
+            label="Payment Date"
+            name="paymentdate"
+            value={date}
+            onChange={(e) => setdate(e.target.value)}
+            autoComplete="paymentdate"
+            autoFocus
+          />
+          <TextField
+          className={classes.textField}
+            // variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            
+            label="Duration"
+            onChange={(e) => setduration(e.target.value)}
+            name="duration"
+            value={duration}
+            autoComplete="Duration"
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Confirm
+          </Button>
+          
         </form>
-      </div>
+        
+      </Container>
+     
       
-    </div>
+    
   );
 }
 export default Dialogbox;
--- src/components/users/Usercomponent.js
+++ src/components/users/Usercomponent.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import Dialogbox from "./Dialogbox";
 import Eventstable from "./Eventstable";
+
+
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+
+import IconButton from '@material-ui/core/IconButton';
+
+import Typography from '@material-ui/core/Typography';
 function Usercomponent() {
   const [dialogboxstatus, setdialogboxstatus] = useState(false);
   const [tableStatus, settableStatus] = useState(false)
   const [events, setevents] = useState([]);
-  
+  const handleClose = () => {
+    setdialogboxstatus(false);
+  };
   // setevents([
   //   ...events,
   //   { event: eventname, date: date, duration: duration },
@@ -37,11 +48,16 @@ function Usercomponent() {
       >
         Details
       </div>
-      {dialogboxstatus ? (
+      <Dialog open={dialogboxstatus} onClose={handleClose} style={{borderRadius:'10'}}>
+        
+      <Dialogbox name={"aaa"} date={"2020-01-01"} duration={1} callBack={handleCallback}/>
+        
+      </Dialog>
+      {/* {dialogboxstatus ? (
         <Dialogbox name={"aaa"} date={"2020-01-01"} duration={1} callBack={handleCallback}/>
       ) : null
       
-      },
+      }, */}
       
       {tableStatus ? (
         <Eventstable state={events}/>
